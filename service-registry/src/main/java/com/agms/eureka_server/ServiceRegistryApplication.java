c88b67b0e6157674d98c87e887527ec7b4958ae8
prashan7070 <prashan7070@gmail.com>
2026-03-05 22:28:02 +0530

    refactor: rename eureka-server to service-registry

--- service-registry/src/main/java/com/agms/eureka_server/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/agms/eureka_server/ServiceRegistryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
 
 }
