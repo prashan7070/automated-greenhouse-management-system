7c3a6b0509fbdc5b0522bd79883484586c416c85
prashan7070 <prashan7070@gmail.com>
2026-03-12 11:06:19 +0530

    feat: add JwtService , entity and repostiory classes

---
+++ auth-service/src/main/java/com/agms/auth_service/repository/UserCredentialRepository.java
@@ -0,0 +1,9 @@
+package com.agms.auth_service.repository;
+
+import com.agms.auth_service.entity.UserCredential;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserCredentialRepository extends JpaRepository<UserCredential, Integer> {
+    Optional<UserCredential> findByUsername(String username);
+}
