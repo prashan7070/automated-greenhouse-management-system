6cda805c6236ae3c77bd6fa2ff08f23c086b79fb
prashan7070 <prashan7070@gmail.com>
2026-03-12 11:29:52 +0530

    feat: implement auth controller in auth service

---
+++ auth-service/src/main/java/com/agms/auth_service/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.agms.auth_service.controller;
+
+import com.agms.auth_service.dto.AuthRequest;
+import com.agms.auth_service.entity.UserCredential;
+import com.agms.auth_service.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService service;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/register")
+    public String addNewUser(@RequestBody UserCredential user) {
+        return service.saveUser(user);
+    }
+
+    @PostMapping("/login")
+    public String getToken(@RequestBody AuthRequest authRequest) {
+        
+        Authentication authenticate = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+
+        if (authenticate.isAuthenticated()) {
+            return service.generateToken(authRequest.getUsername());
+        } else {
+            throw new RuntimeException("Invalid access");
+        }
+    }
+
+}
