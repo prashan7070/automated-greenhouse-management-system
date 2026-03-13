a44a12fd5bb91f4ef34cedab97a6905fa458c64c
prashan7070 <prashan7070@gmail.com>
2026-03-13 07:17:26 +0530

    feat: implement ExternalIotClient using OpenFeign for remote service communication

--- zone-service/src/main/java/com/agms/zone_service/client/ExternalIotClient.java
+++ zone-service/src/main/java/com/agms/zone_service/client/ExternalIotClient.java
@@ -1,4 +1,27 @@
 package com.agms.zone_service.client;
 
-public class ExternalIotClient {
+import com.agms.zone_service.dto.ExternalAuthRequest;
+import com.agms.zone_service.dto.ExternalDeviceRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.Map;
+
+@FeignClient(name = "external-iot-api", url = "${external.iot.base-url}")
+
+public interface ExternalIotClient {
+
+    // Log into External API and get the token
+    @PostMapping("/auth/login")
+    Map<String, Object> login(@RequestBody ExternalAuthRequest request);
+
+    // register the device and get the device ID from the External API
+    @PostMapping("/devices")
+    Map<String, Object> registerDevice(
+            @RequestHeader("Authorization") String token,
+            @RequestBody ExternalDeviceRequest request
+    );
+    
 }
