574fbc9cf2b8456be527dabe8e34a2c5bff6eb8a
prashan7070 <prashan7070@gmail.com>
2026-03-14 09:28:42 +0530

    update all configs for system synchronization

---
+++ sensor-service/src/main/java/com/agms/sensor_service/SensorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agms.sensor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorServiceApplication.class, args);
+	}
+
+}
