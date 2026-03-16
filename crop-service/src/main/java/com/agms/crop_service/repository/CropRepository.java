d80b8c4138ba77ff059d52a7070a4487a73e8cb5
prashan7070 <prashan7070@gmail.com>
2026-03-16 14:14:39 +0530

    feat: implement CropBatch entity, DTO and repository for inventory management

---
+++ crop-service/src/main/java/com/agms/crop_service/repository/CropRepository.java
@@ -0,0 +1,10 @@
+package com.agms.crop_service.repository;
+
+import com.agms.crop_service.entity.CropBatch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CropRepository extends JpaRepository<CropBatch, Integer> {
+    
+}
